4789bc50162e34e89237334bbabb8ebd9ad6279e
kongjun0320 <1741142595@qq.com>
2021-12-03 07:14:51 +0800

    feat(login): add login router
    添加登陆页面

---
+++ src/views/login/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="login-container">
+    <el-form class="login-form">
+      <div class="title-container">
+        <h3 class="title">用户登陆</h3>
+      </div>
+      <el-form-item>
+        <span class="svg-container">
+          <el-icon>
+            <Avatar />
+          </el-icon>
+        </span>
+        <el-input placeholder="username" name="username" type="text">
+        </el-input>
+      </el-form-item>
+      <el-form-item>
+        <span class="svg-container">
+          <el-icon>
+            <Avatar />
+          </el-icon>
+        </span>
+        <el-input placeholder="password" name="password"> </el-input>
+        <span class="show-pwd">
+          <el-icon>
+            <Avatar />
+          </el-icon>
+        </span>
+      </el-form-item>
+      <el-button type="primary" style="width: 100%; margin-bottom: 30px"
+        >登陆</el-button
+      >
+    </el-form>
+  </div>
+</template>
+
+<script setup>
+import { Avatar } from '@element-plus/icons'
+</script>
+
+<style lang="scss" scoped></style>
