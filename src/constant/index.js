64d52f11a25c53a9e3fd2e9f6e0e1e1e4cd6e2ab
kongjun0320 <1741142595@qq.com>
2021-12-28 15:17:32 +0800

    feat(userStore localstorage): 用户登录token处理
    token存储在localstorage

---
+++ src/constant/index.js
@@ -0,0 +1,1 @@
+export const TOKEN = 'aic-admin-token'
--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,16 +1,28 @@
 import md5 from 'md5'
 import { login } from '@/api/sys'
+import { setItem, getItem } from '@/utils/storage'
+import { TOKEN } from '@/constant'
 
 export default {
   namespaced: true,
-  state: () => ({}),
-  mutations: {},
+  state: () => ({
+    token: getItem(TOKEN) || ''
+  }),
+  mutations: {
+    setToken(state, token) {
+      state.token = token
+      setItem(TOKEN, token)
+    }
+  },
   actions: {
-    login(context, userInfo) {
+    login({ commit }, userInfo) {
       const { username, password } = userInfo
       return new Promise((resolve, reject) => {
         login({ username, password: md5(password) })
-          .then((data) => resolve(data))
+          .then((data) => {
+            commit('setToken', data.data.data.token)
+            resolve()
+          })
           .catch((error) => {
             reject(error)
           })
